92e810dc7bdf20795c9bde59f5c7ad3dcccec112
Ray Terrill <rayjohnterrill@gmail.com>
2017-06-08 12:28:35 -0700

    Initialize repo

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+//add middleware to the entire application (every page/request)
+app.use(middleware.logger);
+
+//add middleware to a particular route
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+	console.log('Express server started on port ' + PORT + '!');
+});
